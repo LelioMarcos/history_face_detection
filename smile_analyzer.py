955798f63c83bdd22160c6b67443227b5cacfc9a
LelioMarcos <leliomrcunha@gmail.com>
2025-03-13 12:55:52 -0300

    Comments

--- smile_analyzer.py
+++ smile_analyzer.py
@@ -3,10 +3,10 @@ import pymupdf
 import cv2
 import numpy as np
 import pandas as pd
-from multiprocessing.dummy import Pool as ThreadPool
 from concurrent.futures import ProcessPoolExecutor, as_completed, wait
 import time
 
+# Detect faces using haarcascade
 def detect_faces(img):
     detec = cv2.CascadeClassifier("./haarcascade_frontalface_default.xml")
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -14,6 +14,7 @@ def detect_faces(img):
 
     return faces
 
+# Detect smiles using haarcascade
 def detect_smiles(img):
     smile_cascade = cv2.CascadeClassifier("./haarcascade_smile.xml")
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -22,36 +23,47 @@ def detect_smiles(img):
     return smiles
 
 def detect_faces_in_file(file):
-    FACES_LIMIT = 30 
-    year = file.split('.')[0]
+    FACES_LIMIT = 50
+    year = file.split('.')[0] # The files have their names like <year>.pdf
 
     pdf_path = os.path.join("./yearbooks", file)
     print("Getting faces from", pdf_path)
-    # Create path to store the image of each face
+
     count_faces = 0
-
-    pdf = pymupdf.open(pdf_path)
-
-    for page in pdf:
+    
+    # Iterate for each page
+    for page in pymupdf.open(pdf_path):
+        # Get page as image
         pix = page.get_pixmap()
+
+        # Tranfsorm image to np.arraay 
         img =  np.frombuffer(buffer=pix.samples, dtype=np.uint8).reshape((pix.height, pix.width, 3))
+        
+        # Detect
         faces = detect_faces(img)
 
+        # Create PNG of each detected face
         for i, (x, y, w, h) in enumerate(faces):
             crop = img[y:y+h,x:x+w]
             cv2.imwrite(f"./images/{year}/{page.number}_{i}.png", crop)
             count_faces += 1
 
+            # If reached the limit of faces to detect, stops
+            # reading this file
             if count_faces >= FACES_LIMIT:
                 break
-        
+ 
+        # If reached the limit of faces to detect, stop s
+        # reading this file       
         if count_faces >= FACES_LIMIT:
             break
 
+# For each year, count the number of faces that are smiling
 def detect_smiles_at_year(year):
     files = os.listdir(f"./images/{year}")
     count_smiles = 0
 
+    # For each face, detect smile.
     for file in files:
         img = cv2.imread(f"./images/{year}/{file}")
         smiles = detect_smiles(img)
@@ -69,10 +81,11 @@ def detect_smiles_at_year(year):
 
 if __name__ == '__main__':
     years = []
+    
     nprocs = 4
     files = os.listdir("./yearbooks")
 
-    start = time.time()    
+    # Get years and create folders for each year
     for file in files:
         year = file.split(".")[0]
         years.append(year)
@@ -82,6 +95,9 @@ if __name__ == '__main__':
         if not os.path.isdir(images_path):
             os.makedirs(images_path)
 
+    print("Starting face detection...")
+    
+    # Create PNG file for each detected face
     with ProcessPoolExecutor(max_workers=nprocs) as executor:
         futures = executor.map(
             detect_faces_in_file,
@@ -92,6 +108,7 @@ if __name__ == '__main__':
 
     df_smiles = pd.DataFrame()
 
+    # Create DataFrame containing the smiles anaysis
     with ProcessPoolExecutor(max_workers=nprocs) as executor:
         futures = [executor.submit(detect_smiles_at_year,year) for year in years]
 
@@ -100,7 +117,11 @@ if __name__ == '__main__':
         for result in futures:
             df_smiles = pd.concat([df_smiles, result.result()], ignore_index=True)
 
-    df_smiles = df_smiles.sort_values(by=['year'], ignore_index=True)
+    print("All smiles analysed. Creating CSV..")
 
+    # Sort by year
+    df_smiles = df_smiles.sort_values(by=['year'], ignore_index=True)
     df_smiles.to_csv("smile_analysis.csv", index=False)
+
+    print("All done.")
 
